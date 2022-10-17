622556f1c5ad175cd6ef9e88b7b7b4ab3737de0b
Arman-Kabir <armankabir8@gmail.com>
2022-10-17 16:16:40 +0600

    Jobs schema done

---
+++ models/Jobs.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const validator = require('validator');
+const { ObjectId } = mongoose.Schema.Types;
+
+const jobsSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please provide a name for the job"],
+            trim: true,
+            lowercase: true
+        },
+        description: {
+            type: String,
+            required: [true, "Please provide a description for the job"]
+        },
+        location:{
+            type: String,
+            required: [true, "Please provide a location for the job"]
+        },
+        jobType:{
+            type: String,
+            enum:["full-time","part-time","contractual"],
+            default:"full-time",
+        },
+        salaryRange:{
+            type:Number,
+            required:[true,"Please provide a the salary for the job"]
+        },
+        hiringManager:{
+            name: String,
+            id: {
+                type: ObjectId,
+                ref: 'User'
+            }
+        },
+        appliedCandidates:[{
+            type:ObjectId,
+            ref:"User"
+        }]
+    },
+    {
+        timestamps:true,
+    }
+);
+
+const Jobs = mongoose.model('Jobs',jobsSchema);
+
+module.exports = Jobs;
--- services/user.service.js
+++ services/user.service.js
@@ -4,7 +4,7 @@ const User = require("../models/User")
 exports.signupService = async(userInfo)=>{
     const user = await User.create(userInfo);
     return user;
-}
+};
 
 exports.findUserByEmail = async(email)=>{
     return await User.findOne({email});
