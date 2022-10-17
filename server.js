4ef845315d481e2646b6bb980eef44363c36687d
Arman-Kabir <armankabir8@gmail.com>
2022-10-17 08:29:12 +0600

    db connected

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+const mongoose = require('mongoose');
 const colors = require('colors');
-
+const dotenv = require('dotenv').config();
 
 const app = require('./app');
 
+// DB connection
+mongoose.connect(process.env.DATABASE_LOCAL).then(()=>{
+    console.log(`Database connection is successful` .red.bold);
+})
 
 
 
 // server
-// const PORT = process.env.PORT || 8080;
-const PORT = 5001;
+const PORT = process.env.PORT || 8080;
+// const PORT = 5001;
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`.yellow.bold);
 })
