aed8c89f4f8d94d6907cc7268932acd4a96315e0
Arman-Kabir <armankabir8@gmail.com>
2022-10-20 11:27:53 +0600

    candidate route modified

--- routes/candidate.route.js
+++ routes/candidate.route.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const router = express.Router();
 const candidateController = require('../controllers/candidate.controller');
 const verifyToken = require("../middleware/verifyToken");
 
 
-const router = express.Router();
+
+
+
+
 
 router.get("/jobs",candidateController.getAllJobs);
 router.get("/jobs/:id",candidateController.jobDetailsWithHrInfo);
 router.post("/jobs/:id/apply", verifyToken,  candidateController.applyJob);
-
 
 module.exports = router;
