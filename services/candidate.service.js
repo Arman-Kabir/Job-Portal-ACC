3340037de1d2b50eb5fee0f6211bcf8e714ea045
Arman-Kabir <armankabir8@gmail.com>
2022-10-20 18:55:30 +0600

    if applied can't apply again done

--- services/candidate.service.js
+++ services/candidate.service.js
@@ -1,4 +1,6 @@
 // const { updateMany } = require("../models/Jobs");
+const { WriteError } = require("mongodb");
+const { Error, MongooseError } = require("mongoose");
 const Jobs = require("../models/Jobs");
 const User = require("../models/User");
 
@@ -17,30 +19,32 @@ exports.jobDetailsWithHrInfoService = async (id) => {
 
 exports.applyJobService = async (id, file, user) => {
     // const job = await ;
-    console.log(user.id);
+    // console.log(user.id);
 
     const isApplied = await Jobs.find({ _id: id, 'appliedCandidates': user.id });
-    console.log(isApplied);
-    // const [ appliedCandidates ] = await isApplied;
+    // console.log(isApplied);
+
+    if (isApplied.length == 0) {
+        console.log("isEmpty");
+    } else {
+        console.log("Not Empty");
+    }
+
+    if (isApplied.length > 0) {
+        return "You applied to this job before";
+    } else {
+        const res = await Jobs.updateOne(
+            { _id: id },
+            { $push: { appliedCandidates: user.id } }
+        );
+
+        const res2 = await User.updateOne(
+            { _id: user.id },
+            { $push: { resume: { data: file.filename } } }
+        );
+
+        return [res, res2];
+    }
 
 
-    // console.log(appliedCandidates);
-
-
-
-    // const unq = isApplied.appliedCandidate.findIndex(user.id);
-    // console.log(unq);
-
-    // const res = await Jobs.updateOne(
-    //     { _id: id },
-    //     { $push: { appliedCandidates: user.id } }
-    // );
-
-    // const res2 = await User.updateOne(
-    //     { _id: user.id },
-    //     { $push: { resume: { data: file.filename } } }
-    // );
-
-
-    // return [res, res2];
 }
