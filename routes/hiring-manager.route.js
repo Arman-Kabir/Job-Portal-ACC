61ffc4b73524155c0c5fc4b3095a2f86595b1ffa
Arman-Kabir <armankabir8@gmail.com>
2022-10-21 14:59:02 +0600

    create job token verify added

--- routes/hiring-manager.route.js
+++ routes/hiring-manager.route.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const hiring_managerController = require('../controllers/hiring-manager.controller');
-const  authorization =  require("../middleware/authorization");
+const authorization = require("../middleware/authorization");
+const verifyToken = require("../middleware/verifyToken");
 
 
 const router = express.Router();
 
-router.post("/jobs", authorization("hiring-manager"),  hiring_managerController.createJob)
+router.post("/jobs", verifyToken, authorization("hiring-manager"), hiring_managerController.createJob)
 
-router.get("/manager/jobs",hiring_managerController.getJobsOfHr)
+router.get("/manager/jobs", hiring_managerController.getJobsOfHr)
 
-router.get("/manager/jobs/:id",hiring_managerController.getJobDetails)
+router.get("/manager/jobs/:id", hiring_managerController.getJobDetails)
 
 
 
-router.patch("/jobs/:id",hiring_managerController.updateJob)
+router.patch("/jobs/:id", hiring_managerController.updateJob)
 
 
 module.exports = router;
